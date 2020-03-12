bcd01ef58a566d012af508c92dfa75e3eac83a0e
Ricardo Catalinas Jiménez <r@untroubled.be>
2020-03-12 16:09:47 +0000

    Revert "Add Destroy method to Snapshot object"
    This reverts commit b3f23888b69041ae32c2525ba4a03a0f3d58e5bb.

--- snapshot.go
+++ snapshot.go
@@ -3,8 +3,6 @@ package grocksdb
 // #include "rocksdb/c.h"
 import "C"
 
-import "unsafe"
-
 // Snapshot provides a consistent view of read operations in a DB.
 type Snapshot struct {
 	c *C.rocksdb_snapshot_t
@@ -13,10 +11,4 @@ type Snapshot struct {
 // NewNativeSnapshot creates a Snapshot object.
 func NewNativeSnapshot(c *C.rocksdb_snapshot_t) *Snapshot {
 	return &Snapshot{c}
-}
-
-// Destroy deallocates the Snapshot object.
-func (snapshot *Snapshot) Destroy() {
-	C.rocksdb_free(unsafe.Pointer(snapshot.c))
-	snapshot.c = nil
 }
